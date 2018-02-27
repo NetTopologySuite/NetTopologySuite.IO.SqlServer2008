08ffedc38d96187cd3087ec29affdf039fea5e85
Ben Abelshausen <ben@osmsharp.com>
2018-02-27 09:47:37 +0000

    Migrated strong naming test.

---
+++ NetTopologySuite.IO.SqlServer2008.Test/Issue174.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTopologySuite.IO.SqlServer2008.Test
+{
+    class Issue174
+    {
+        [Test, Category("Issue174")]
+        public void ensure_NetTopologySuite_IO_SqlServer2008_assembly_is_strongly_named()
+        {
+            AssertStronglyNamedAssembly(typeof(MsSql2008GeometryReader));
+        }
+
+        private void AssertStronglyNamedAssembly(Type typeFromAssemblyToCheck)
+        {
+            Assert.IsNotNull(typeFromAssemblyToCheck, "Cannot determine assembly from null");
+            Assembly assembly = typeFromAssemblyToCheck.Assembly;
+            StringAssert.DoesNotContain("PublicKeyToken=null", assembly.FullName, "Strongly named assembly should have a PublicKeyToken in fully qualified name");
+        }
+    }
+}
